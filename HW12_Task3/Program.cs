d8c0714283977fd5b3eeec1b831caaf800aee4d0
Vladislav-v2 <16fi.v.lohai@str.npu.edu.ua>
2020-04-22 23:45:59 +0300

    Добавьте файлы проекта.

---
+++ HW12_Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HW12_Task3
+{
+    public class Program
+    {
+        static void Main(string[] arg)
+        {
+            Console.ReadKey();
+        }
+
+        public static double Result(string str)
+        {
+            Regex r = new Regex(@"-{0,1}\d+");
+            MatchCollection m = r.Matches(str);
+            List<int> numbers = new List<int>();
+            foreach (Match item in m)
+            {
+                numbers.Add(int.Parse(item.Value));
+            }
+            double result = 0;
+            try
+            {
+                result = numbers[0] + numbers[1] * numbers[2] /
+                   Math.Pow(numbers[3] + numbers[4], numbers[5]);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divide by zero");
+            }
+            return result;
+        }
+    }
+}
---
+++ HW12_Task3Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using HW12_Task3;
+
+namespace HW12_Task3Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [TestCase("3+4*2/(1-5)^2", 3.5)]
+        [TestCase("5+8*3/(19-6)^0", 29)]
+        [TestCase("3+5*2/(3-4)^2", 13)]
+        [TestCase("2+34*7/(7-12)^2", 11.52)]
+        public void TestResultMethod(string str, double expRes)
+        {
+            double actRes = Program.Result(str);
+            Assert.AreEqual(expRes, actRes);
+        }
+    }
+}
